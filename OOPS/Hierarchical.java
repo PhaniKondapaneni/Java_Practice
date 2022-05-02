9edd7e18afd2186583f9fa7d4817d4e8dfbe1839
PhaniKondapaneni <94769966+PhaniKondapaneni@users.noreply.github.com>
2022-05-02 17:59:01 +0530

    Add files via upload

---
+++ OOPS/Hierarchical.java
@@ -0,0 +1,65 @@
+/*class Person{
+  String personName;
+}
+class Student extends Person
+{
+  String sName;
+  int sId;
+  String sDept;
+}
+class Employee extends Person
+  {
+    String eName;
+    int eId;
+    String eDept;
+  }
+class Graduate extends Student
+  {
+    String eName;
+    int eId;
+    String eDept;
+  }
+class PostGraduate extends Student
+  {
+    String eName;
+    int eId;
+    String eDept;
+  }
+class Faculty extends Employee
+  {
+    String eName;
+    int eId;
+    String eDept;
+  }
+class Staff extends Employee
+  {
+    String eName;
+    int eId;
+    String eDept;
+  }
+class Masters extends PostGraduate
+  {
+    String mName;
+    int mId;
+    String mDept;
+  }
+class Phd extends PostGraduate
+  {
+    String phName;
+    int phId;
+    String phDept;
+  }
+import java.util.Scanner;
+
+class Hierarchical
+  {
+    public static void main(String args[])
+    {
+      Scanner sc = new Scanner(System.in);
+      Masters = new Masters();
+      Phd = new Phd();
+      Faculity = new Faculty();
+      Staff = new Staff();
+      
+    }
+  }*/
